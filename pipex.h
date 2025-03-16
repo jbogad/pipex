b8fc708070cd3b4e6fc265371db7f08e60b8447b
jbogad <javier6580@gmail.com>
2025-03-16 13:47:38 +0100

    ready to check

---
+++ pipex.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 13:11:43 by marvin            #+#    #+#             */
+/*   Updated: 2025/03/16 13:11:43 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+/* Mandatory functions */
+void	error(void);
+char	*find_path(char *cmd, char **envp);
+int		get_next_line(char **line);
+void	execute(char *argv, char **envp);
+
+#endif
